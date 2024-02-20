dc74cf3b0a570adae4b95861bcaf7d7189ff9d79
dyefim <yefimchuk.dmytro@gmail.com>
2024-02-20 13:50:32 +0200

    Accept cli argument to customize greeting

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-readline.question('Who are you? \n', name => {
-  console.log(`Hey there ${name}!`);
+const GREETING_PARAM_KEY = '--greeting';
+
+let greeting = 'Hey there';
+
+const args = process.argv.slice(2);
+
+if (args[0].includes(GREETING_PARAM_KEY + '=')) {
+  greeting = args[0].replace(GREETING_PARAM_KEY + '=', '');
+}
+
+readline.question('Who are you? \n', (name) => {
+  console.log(`${greeting} ${name}!`);
   readline.close();
 });
