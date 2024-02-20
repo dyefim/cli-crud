84720defea16dcc7be0a1e63bec107706f15ff62
dyefim <yefimchuk.dmytro@gmail.com>
2024-02-20 18:50:20 +0200

    Add ability to create todo items

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 const pg = require('pg');
 const { Client } = pg;
 
-const args = process.argv.slice(2);
-const [, tableName] = args[0].match(/--table=(\S+)/);
+const argv = yargs(hideBin(process.argv)).argv;
 
 (async () => {
   const client = new Client();
   await client.connect();
 
-  const res = await client.query(`SELECT * from ${tableName}`);
+  const queryText = `INSERT into list(
+    name
+  ) VALUES($1) RETURNING *`;
+
+  const values = [argv.new];
+
+  const res = await client.query(queryText, values);
+
   console.log(res.rows);
   await client.end();
 })();
